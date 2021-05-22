3baa4260eddfedb116d950c05751d31142f6aba4
Syed Ali Murtaza <ali@kazmi.me>
2021-05-22 12:14:43 +0500

    Updates service provider

--- Providers/MainServiceProvider.php
+++ Providers/MainServiceProvider.php
@@ -10,16 +10,16 @@ class MainServiceProvider extends MainProvider
     public function boot(): void
     {
         // To use customize passport models to replace primary key type from auto-increment to uuid.
-        Passport::useClientModel(\App\Containers\Vendor\Uuider\Models\Client::class);
-        Passport::usePersonalAccessClientModel(\App\Containers\Vendor\Uuider\Models\PersonalAccessClient::class);
+        Passport::useClientModel(config('uuider.models.client',\App\Containers\Vendor\Uuider\Models\Client::class));
+        Passport::usePersonalAccessClientModel(config('uuider.models.personal_access_client',\App\Containers\Vendor\Uuider\Models\PersonalAccessClient::class));
     }
 
     public function register(): void
     {
         // Over-write permission configuration on run time to
         // update role and permissions models
-        config(['permission.models.permission' => config('uuider.models.permission', \App\Containers\Vendor\Uuider\Models\Permission::class)]);
-        config(['permission.models.role' => config('uuider.models.role', \App\Containers\Vendor\Uuider\Models\Role::class)]);
+        config(['permission.models.permission' => config('beaner.models.permission', \App\Containers\Vendor\Beaner\Models\Permission::class)]);
+        config(['permission.models.role' => config('beaner.models.role', \App\Containers\Vendor\Beaner\Models\Role::class)]);
     }
 
 }
